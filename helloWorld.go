a6ba7a4784f6964fde1b2129e473581c9919cf9b
Lucasfrota <frota_lv@hotmail.com>
2017-11-20 20:57:31 -0400

    go recipes

---
+++ helloWorld.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+      "fmt"
+      "time"
+        )
+
+func main(){
+  fmt.Printf("Hello World\n")
+
+  fmt.Printf("Now it is: ", time.Now())
+}
---
+++ pacotes.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+  "fmt"
+  "math/rand"
+)
+
+func main(){
+  fmt.Printf("My favorit number is ",
+  rand.Intn(10))
+}
